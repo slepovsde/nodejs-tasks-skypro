bae382dd63fedcc9c01702e0aa2e0cfd0e11008b
Dmitry Slepov <91319920+slepovsde@users.noreply.github.com>
2021-10-21 15:02:13 +0700

    Update app.js

--- kin-dza-dza/app.js
+++ kin-dza-dza/app.js
@@ -1,23 +1,20 @@
+languageLang = process.env.LANG
 
-function translateKinDzaDza (LANG) {
-switch (LANG) {
+function translateKinDzaDza () {
+switch (languageLang) {
     case 'EN':
-    LANG = 'en_US.UTF-8' 
     console.log('The Pacak must do ku twice')
     break
 
     case 'UK': 
-    LANG = 'uk_UA.UTF-8'
     console.log('Пацак повинен робити ку два рази')
     break
 
     case 'DE':
-    LANG = 'de_DE.utf8'
     console.log('Das Pazak muss ku zweimal machen')
     break
 
     default:
-    LANG = 'ru_RU.UTF-8'
     console.log('Пацак должен делать ку два раза')
     break
 }
